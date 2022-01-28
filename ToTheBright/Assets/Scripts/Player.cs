24a72b3fbf320e67c3e49f16910b7a4b9f8c24bf
JaSamCody <tin.pavlinec2003@gmail.com>
2022-01-28 22:44:08 +0100

    anim

--- ToTheBright/Assets/Scripts/Player.cs
+++ ToTheBright/Assets/Scripts/Player.cs
@@ -5,37 +5,67 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    private Vector2 moveDirection;
-    private Vector2 lastMoveDirection;
+    public float jump;
+    public float horizontal;
+
+
+    public float jumpAmount = 35;
+    public float gravityScale = 10;
+    public float fallingGravityScale = 40;
+
+
     public Rigidbody2D rb;
     public Animator anim;
-  
+
+
+    
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInputs();
-        Animate();
-    }
+        horizontal = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetAxisRaw("Jump");
 
-    public void ProcessInputs()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        if ((moveX == 0 && moveY == 0) && (moveDirection.x != 0 || moveDirection.y != 0))
+        if (Input.GetKey(KeyCode.D))
         {
-            lastMoveDirection = moveDirection;
+            anim.SetInteger("AnimState", 1);
         }
 
-        moveDirection = new Vector2(moveX, moveY);
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetInteger("AnimState", 2);
+        }
+
+        else
+        {
+            anim.SetInteger("AnimState", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
+        if (rb.velocity.y >= 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallingGravityScale;
+        }
     }
+
+    
     private void FixedUpdate()
     {
         Move();
@@ -43,16 +73,10 @@ public class Player : MonoBehaviour
 
     public void Move()
     {
-        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    public void Animate()
-    {
-        anim.SetFloat("AnimMoveX", moveDirection.x);
-        anim.SetFloat("AnimMoveY", moveDirection.y);
-        anim.SetFloat("AnimMoveMagnitude", moveDirection.magnitude);
-        anim.SetFloat("AnimLastMoveX", lastMoveDirection.x);
-        anim.SetFloat("AnimLastMoveY", lastMoveDirection.y);
-    }
+    
+    
 
 }
