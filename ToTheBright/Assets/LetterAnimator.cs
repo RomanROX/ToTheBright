c6ad5926a0274c799009511b396ef4b6ff4ace32
MNinna <80454202+MNinna@users.noreply.github.com>
2022-01-29 15:47:02 +0100

    SunMenu

---
+++ ToTheBright/Assets/LetterAnimator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterAnimator : MonoBehaviour
+{
+    public float WaitBetween = 0.5f;
+    List<Animator> animators;
+    void Start()
+    {
+        animators = new List<Animator>(GetComponentsInChildren<Animator>());
+        StartCoroutine(DoAnimation());
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while(true)
+        {
+            foreach (var animator in animators)
+            {
+                animator.SetTrigger("DoAnimation");
+                yield return new WaitForSeconds(WaitBetween);
+            }
+        }
+    }
+}
